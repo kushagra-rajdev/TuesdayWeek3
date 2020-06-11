6c0b21accd05150910e5ffcb0c3d8e3e8516dc9d
Pate1198 <pate2516@sheridancollege.ca>
2020-06-10 22:48:50 -0400

    pushing msg

---
+++ src/week3/Week3.java
@@ -0,0 +1,17 @@
+package week3;
+
+/**
+ * This class +++Insert Description Here+++
+
+ * @author Jay Patel
+ */
+public class Week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Pushing a Message");
+    }
+
+}
